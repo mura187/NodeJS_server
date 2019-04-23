6babf429a34405285de13bdb01f1252b30315312
mura187 <muratseit@gmail.com>
2019-04-23 20:58:53 +0600

    mongoDB connected,read,readOne,post

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var app = express();
 var bodyParser =  require('body-parser'); //парсит тело запроса  и в request body записывает, что мы передали
+var mongoose = require('mongoose');
+var ObjectID = mongoose.Types.ObjectId;
+var db;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
@@ -37,25 +40,39 @@ app.get('/', function(req, res){
 });
 
 app.post('/product', function (req, res){
-    var product = {
-        id: Date.now(), //change to another generation!!!
+    var product = { 
         name: req.body.name
-    };
-    products.push(product); 
-    res.send(product);
+    }; 
+    db.collection('products').insert(product, 
+        function(err, result){
+            if(err){
+                console.log(err);
+                return res.sendStatus(500);
+            } 
+            res.send(product);
+    });
 });
 
 app.get('/product', function (req, res){
-    res.send(products);
+    //res.send(products);
+    db.collection('products').find().toArray(function(err, docs){
+            if (err){
+                console.log(err);
+                return res.sendStatus(500);
+            }
+            res.send(docs)
+    })
 });
 
 app.get('/product/:id', function(req, res){
-    console.log(req.params);
-    var product = products.find(
-        function(product){
-            return product.id === Number(req.params.id);
-    })
-    res.send(product);
+    db.collection('products').findOne({_id: ObjectID(req.params.id)}, 
+        function(err, doc){
+            if(err){
+                console.log(err);
+                return res.sendStatus(500);
+            }
+        res.send(doc);
+    });
 });
 
 app.put('/product/:id', function(req, res){
@@ -75,7 +92,14 @@ app.delete('/product/:id', function(req, res){
     res.sendStatus(200);
 })
 
-
-app.listen(3012, function(){
-    console.log('api started');
+mongoose.connect("mongodb://localhost:27017/api", { useNewUrlParser: true }, function(err, database) {
+    if (err) {
+        return console.log(err);
+    }
+    db = database;
+    app.listen(3003,function () {
+        console.log('api started, connection w/ db success')
+    })
 });
+
+
